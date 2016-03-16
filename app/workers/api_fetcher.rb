81131f8a189b5784e5c4f20eedd470e5e0884e30
Joran <joran.k@gmail.com>
2016-03-17 11:43:45 +1300

    split tasks

--- app/workers/api_fetcher.rb
+++ app/workers/api_fetcher.rb
@@ -3,6 +3,20 @@ class ApiFetcher
   sidekiq_options unique: :until_timeout, unique_expiration: 1 * 60 # 2 hours
 
   def perform(query)
+    do_twitter(query)
+    do_instagram(query)
+  end
+
+
+
+  private
+
+
+  def do_instagram query
+
+  end
+
+  def do_twitter query
     @twitter ||= Twitter::REST::Client.new do |config|
      config.consumer_key        = ENV["twitter_consumer_key"]
      config.consumer_secret     = ENV["twitter_consumer_secret"]
@@ -10,11 +24,15 @@ class ApiFetcher
      config.access_token_secret = ENV["twitter_access_token_secret"]
     end
 
-    @twitter_results = Rails.cache.fetch(query, :expires_in => 1.hours) do
-      @twitter.search("#{query} -rt", result_type: "recent").take(50)
+    @twitter_results = Rails.cache.fetch(query, :expires_in => 10.minutes) do
+      @twitter.search("#{query} -rt", result_type: "recent").take(100)
     end
 
+    puts "Tweets: #{@twitter_results.length}"
+
     @twitter_results.each do |tr|
+      print 'T'
+
       t = Tweet.find_or_initialize_by(tweet_id: tr.id)
       t.data = tr.to_json
 
@@ -23,6 +41,5 @@ class ApiFetcher
       end
       t.save!
     end
-
   end
 end
--- lib/tasks/elasticsearch.rake
+++ lib/tasks/elasticsearch.rake
@@ -2,10 +2,18 @@ require 'elasticsearch/rails/tasks/import'
 
 
 task :refresh_es => :environment do
+  # bring in new content
+  Rake::Task["suck_in"].invoke
+  
   [ Tweet, Story ].each do |entity|
     entity.__elasticsearch__.client.indices.delete index: entity.index_name rescue nil
     entity.__elasticsearch__.create_index! force: true
     entity.__elasticsearch__.refresh_index!
     entity.import
   end
+end
+
+task :suck_in => :environment do
+  # bring in new content
+  ApiFetcher.perform_async("#makerhood")
 end
