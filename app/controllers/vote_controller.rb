2bd7f7a3c690df4962c56bdbfb584ad588ebaefb
Joran <joran.k@gmail.com>
2016-03-17 20:57:54 +1300

    vote on all types, not just tweets

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -1,9 +1,19 @@
 class VoteController < ApplicationController
 
   def vote_up
-    tweet = Tweet.find(params[:id])
-    tweet.vote!(request.ip)
-
-    @tweet = tweet
+    case params[:type]
+    when 'tweet'
+      t = Tweet.find(params[:id])
+      t.vote!(request.ip)
+      @tweet = t
+    when 'instagram'
+      i = Instagram.find(params[:id])
+      i.vote!(request.ip)
+      @tweet = i
+    else
+      s = Story.find(params[:id])
+      s.vote!(request.ip)
+      @tweet = s
+    end
   end
 end
